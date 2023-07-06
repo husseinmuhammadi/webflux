2a6c213077a8ece72546cd7cd1965c1b03637cec
Hossein Mohammadi <hossein.mohammadi@al.nl>
2023-07-06 23:49:51 +0200

    Enable reactive

--- web/src/main/java/com/javastudio/reactive/configuration/WebFluxConfig.java
+++ web/src/main/java/com/javastudio/reactive/configuration/WebFluxConfig.java
@@ -1,11 +1,11 @@
 package com.javastudio.reactive.configuration;
 
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.reactive.config.EnableWebFlux;
-//import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-//@EnableWebFlux
-public class WebFluxConfig /*implements WebFluxConfigurer*/ {
+@EnableWebFlux
+public class WebFluxConfig implements WebFluxConfigurer {
 
 }
